e1eec4efdb435ddedbd6c406e2777e254829525e
unknown <katibis@ufl.edu>
2023-10-24 13:19:09 -0400

    This is the main code for Lab 6.

---
+++ Lab 6.py
@@ -0,0 +1,30 @@
+def encode(p):
+    temp = ""
+    for i in p:
+        temp += str((int(i) + 3) % 10)
+    return temp
+
+def decode(p):
+    temp = ""
+    for i in p:
+        temp += str((int(i) + 7) % 10)
+    return temp
+
+if __name__ == "__main__":
+    while True:
+        print("Menu")
+        print("------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print("")
+        print("Please enter an option: ")
+        choice = int(input())
+        if choice == 1:
+            print("Please enter your password to encode: ", end = "")
+            password = encode(input())
+            print("Your password has been encoded and stored!")
+        if choice == 2:
+            print("The encoded password is " + str(password) + ", and the original password is " + decode(password) + ".")
+        if choice == 3:
+            break
